6140d47bbea28a81f2235ecc05f6d263a7fbea1e
Ahmet Sulu <ahmet.sulu1993@gmail.com>
2024-07-31 16:17:29 +0300

    Add project files.

---
+++ Encapsulation/CarApp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Car
+    {
+        public string Brand { get; set; } // Herhangi bir veri doğrulama gerekmediği için boş bırakılıyor.
+        public double Model { get; set; }
+        public string Colour { get; set; }
+
+        private int _doorCount; // Kapsüllenmiş özellik
+
+        // Kapı Sayısı özelliği için özel get ve set
+        public int DoorCount 
+        {
+            get { return _doorCount; }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _doorCount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz kapı sayısı.(-1).");
+                    _doorCount = -1;
+                }
+            }
+        }
+        // Yapıcı metot (Constructor)
+        public Car (string brand, double model, string colour, int _doorCount)
+        {
+            Brand = brand;
+            Model = model;
+            Colour = colour;
+            DoorCount = _doorCount; // Kapsülleme işlemini kontrol eder
+        }
+
+        // Bilgileri yazdıran Metot
+        public void ArabaBilgileriniYazdır()
+        {
+            Console.WriteLine($"Marka: {Brand}, Model: {Model}, Renk: {Colour}, Kapı Sayısı: {DoorCount}");
+        }
+    }
+
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿using Encapsulation;
+
+
+// Gecerli kapı sayısı
+Car car = new Car ("BMW", 5.20, "White", 4);
+car.ArabaBilgileriniYazdır();
+
+
+// Gecersiz kapı sayısı
+Car car1 = new Car("BMW", 5.20, "SpaceGray", 3);
+car1.ArabaBilgileriniYazdır();
